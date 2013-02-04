11235f68d2b3c0a062c044a41211791ab5afee7b
Stephen Jothen <sjothen@gmail.com>
2013-02-04 16:58:40 +0100

    Allow running creme on a file

--- src/main.ml
+++ src/main.ml
@@ -2,18 +2,6 @@ module P = Parser
 module L = Lexer
 module E = Eval
 
-let rec repl prompt buf =
-  print_string prompt; flush stdout;
-  let tok = P.main L.token buf in
-  let evd = E.eval tok in
-  Creme.creme_print evd;
-  repl prompt buf
-
-let rec load chan =
-  let tok = P.main L.token chan in
-  ignore (E.eval tok);
-  load chan
-
 let perror = Printf.printf
 let ctos c = Creme.creme_to_string c
 
@@ -22,6 +10,23 @@ let print_exn e =
   match e with
   | E.Creme_error s -> perror "%s\n" s
   | x               -> perror "unhandled exception %s\n" (Printexc.to_string x)
+
+let rec repl prompt buf =
+  print_string prompt; flush stdout;
+  let tok = P.main L.token buf in
+  let evd = E.eval tok in
+  Creme.creme_print evd;
+  repl prompt buf
+
+let load fname =
+  let rec aux c =
+    let tok = P.main L.token c in
+    ignore (E.eval tok);
+    aux c
+  in
+  try
+    aux (Lexing.from_channel (open_in fname))
+  with L.Eof -> () | x -> print_exn x
 
 let rec repl_ f =
   try
@@ -32,8 +37,10 @@ let rec repl_ f =
 
 let _ =
   E.define_base ();
-  try
-    load (Lexing.from_channel (open_in "boot/base.crm"))
-  with
-  | L.Eof -> repl_ (fun () -> repl "> " (Lexing.from_channel stdin))
-  | x     -> print_exn x
+  (try
+    load "boot/base.crm"
+  with L.Eof -> () | x -> print_exn x);
+  if Array.length Sys.argv > 1 then
+    load Sys.argv.(1)
+  else
+    repl_ (fun () -> repl "> " (Lexing.from_channel stdin))
