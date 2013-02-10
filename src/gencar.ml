db2cc54dd0f24c6edec02eb0f35ebb8920ea2066
Stephen Jothen <sjothen@gmail.com>
2013-02-10 10:36:06 +0100

    Use .[] instead of String.get

--- src/gencar.ml
+++ src/gencar.ml
@@ -3,7 +3,7 @@ module L = List
 
 let last s =
   let len = S.length s in
-  (S.sub s 0 (len - 1), S.get s (len - 1))
+  (S.sub s 0 (len - 1), s.[len - 1])
 
 let rec generateform name =
   match last name with
